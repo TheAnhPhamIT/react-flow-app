43071178fd600f0daeaa8b03087c910c5cdcb062
anh pham <theanhphamit@gmail.com>
2024-05-28 11:29:58 +0700

    add default node data and edge data when load app

--- src/modules/workflow/components/edges/index.ts
+++ src/modules/workflow/components/edges/index.ts
@@ -1,33 +1,199 @@
-import { MarkerType, type Edge, type EdgeTypes } from "reactflow";
-import FloatingEdge from "./FloatingEdge/FloatingEdge";
-import DashEdge from "./DashEdge/DashEdge";
+import { Edge, MarkerType, type EdgeTypes } from 'reactflow';
+import FloatingEdge from './FloatingEdge/FloatingEdge';
+import DashEdge from './DashEdge/DashEdge';
 
 export const initialEdges = [
-  {
-    id: "1-2",
-    source: "1",
-    target: "2",
-    type: "custom",
-    markerEnd: { type: MarkerType.Arrow },
-  },
-  {
-    id: "2-4",
-    source: "2",
-    target: "4",
-    type: "custom",
-    markerEnd: { type: MarkerType.Arrow },
-  },
-  {
-    id: "4-3",
-    source: "4",
-    target: "3",
-    type: "custom",
-    markerEnd: { type: MarkerType.Arrow },
-  },
+    {
+        id: 'edge-1716869202295',
+        type: 'custom',
+        source: 'node-1716869178889',
+        target: 'node-1716869202295',
+        markerEnd: {
+            type: MarkerType.Arrow,
+        },
+        data: {
+            label: 'edge',
+        },
+        selected: false,
+    },
+    {
+        id: 'edge-1716869265766',
+        type: 'dash',
+        source: 'node-1716869202295',
+        target: 'node-1716869265766',
+        markerEnd: {
+            type: MarkerType.Arrow,
+        },
+        data: {
+            label: 'edge',
+        },
+    },
+    {
+        source: 'node-1716869202295',
+        sourceHandle: 'bottom',
+        target: 'node-1716869419367',
+        targetHandle: 'top',
+        id: 'edge-1716869423077',
+        type: 'custom',
+        markerEnd: {
+            type: MarkerType.Arrow,
+        },
+        data: {
+            label: 'edge',
+        },
+    },
+    {
+        id: 'edge-1716869456014',
+        type: 'custom',
+        source: 'node-1716869419367',
+        target: 'node-1716869456014',
+        markerEnd: {
+            type: MarkerType.Arrow,
+        },
+        data: {
+            label: 'edge',
+        },
+    },
+    {
+        id: 'edge-1716869532373',
+        type: 'custom',
+        source: 'node-1716869456014',
+        target: 'node-1716869532373',
+        markerEnd: {
+            type: MarkerType.Arrow,
+        },
+        data: {
+            label: 'edge',
+        },
+    },
+    {
+        id: 'edge-1716869553093',
+        type: 'custom',
+        source: 'node-1716869456014',
+        target: 'node-1716869553093',
+        markerEnd: {
+            type: MarkerType.Arrow,
+        },
+        data: {
+            label: 'edge',
+        },
+    },
+    {
+        source: 'node-1716869532373',
+        sourceHandle: 'bottom',
+        target: 'node-1716869553093',
+        targetHandle: 'top',
+        id: 'edge-1716869568669',
+        type: 'custom',
+        markerEnd: {
+            type: MarkerType.Arrow,
+        },
+        data: {
+            label: 'edge',
+        },
+    },
+    {
+        id: 'edge-1716869577709',
+        type: 'custom',
+        source: 'node-1716869553093',
+        target: 'node-1716869577709',
+        markerEnd: {
+            type: MarkerType.Arrow,
+        },
+        data: {
+            label: 'edge',
+        },
+    },
+    {
+        id: 'edge-1716869631670',
+        type: 'custom',
+        source: 'node-1716869577709',
+        target: 'node-1716869631669',
+        markerEnd: {
+            type: MarkerType.Arrow,
+        },
+        data: {
+            label: 'edge',
+        },
+    },
+    {
+        id: 'edge-1716869742878',
+        type: 'custom',
+        source: 'node-1716869631669',
+        target: 'node-1716869742878',
+        markerEnd: {
+            type: MarkerType.Arrow,
+        },
+        data: {
+            label: 'edge',
+        },
+    },
+    {
+        id: 'edge-1716869860284',
+        type: 'custom',
+        source: 'node-1716869202295',
+        target: 'node-1716869860284',
+        markerEnd: {
+            type: MarkerType.Arrow,
+        },
+        data: {
+            label: 'edge',
+        },
+    },
+    {
+        id: 'edge-1716870052717',
+        type: 'custom',
+        source: 'node-1716869860284',
+        target: 'node-1716870052717',
+        markerEnd: {
+            type: MarkerType.Arrow,
+        },
+        data: {
+            label: 'edge',
+        },
+    },
+    {
+        source: 'node-1716869742878',
+        sourceHandle: 'top',
+        target: 'node-1716870052717',
+        targetHandle: 'bottom',
+        id: 'edge-1716870077556',
+        type: 'custom',
+        markerEnd: {
+            type: MarkerType.Arrow,
+        },
+        data: {
+            label: 'edge',
+        },
+    },
+    {
+        id: 'edge-1716870094205',
+        type: 'custom',
+        source: 'node-1716870052717',
+        target: 'node-1716870094205',
+        markerEnd: {
+            type: MarkerType.Arrow,
+        },
+        data: {
+            label: 'edge',
+        },
+    },
+    {
+        id: 'edge-1716870110324',
+        type: 'custom',
+        source: 'node-1716869742878',
+        target: 'node-1716870110324',
+        markerEnd: {
+            type: MarkerType.Arrow,
+        },
+        data: {
+            label: 'edge',
+        },
+    },
 ] satisfies Edge[];
 
 export const edgeTypes = {
-  custom: FloatingEdge,
-  floating: FloatingEdge,
-  dash: DashEdge,
+    custom: FloatingEdge,
+    floating: FloatingEdge,
+    dash: DashEdge,
 } satisfies EdgeTypes;
